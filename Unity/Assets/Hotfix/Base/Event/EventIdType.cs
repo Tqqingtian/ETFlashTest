afdd2bfe02da4323aeea625e034fa58390d20f37
TQ <937489684>
2019-12-11 20:13:18 +0800

    播放器测试

---
+++ Unity/Assets/Hotfix/Base/Event/EventIdType.cs
@@ -0,0 +1,12 @@
+﻿namespace ETHotfix
+{
+	public static class EventIdType
+	{
+		public const string InitSceneStart = "InitSceneStart";
+		public const string LoginFinish = "LoginFinish";
+        /// <summary>
+        /// 进入地图
+        /// </summary>
+		public const string EnterMapFinish = "EnterMapFinish";
+	}
+}
